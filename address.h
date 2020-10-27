f712d078958af57ac76ce3d2f05d41a6e19adb65
Rosy-Ji <jiruijie6@gmail.com>
2020-10-27 14:29:32 -0400

    Update address header file

--- address.h
+++ address.h
@@ -15,23 +15,23 @@ class Address {
 public:
 
 	Address(); //Default Constructor
-	Address(string longitude, string latitude, string addressName); //Constructor
-	string getLongitude(); //Accessor to return longitude
-	string getLatitude(); //Accessor to return latitude
+	Address(float longitude, float latitude, string addressName); //Constructor
+	float getLongitude(); //Accessor to return longitude
+	float getLatitude(); //Accessor to return latitude
 	string getAddressName(); //Accessor to return address name
 
-	void setLongitude(string longitude); //Mutator to set the longitude
-	void setLatitude(string latitude); //Mutator to set the latitude
+	void setLongitude(float longitude); //Mutator to set the longitude
+	void setLatitude(float latitude); //Mutator to set the latitude
 	void setAddressName(string addressName); //Mutator to set the address name
 
-	void printAddress(string longitude, string latitude, string addressName); //Method to print address
+	void printAddress(float longitude, float latitude, string addressName); //Method to print address
 
 	long double toRadians(const long double);// Utility function for converting degrees to radians
 	float calculateDistance(float longitude, float latitude, float myLongitude, float myLatitude);// Method that calculates the distance between 2 points with coordinates (longitude, latitude)
 	bool deleteAddress(string address);// Delete address method
 
 private:
-	string longitude;
-	string latitude;
+	float longitude;
+	float latitude;
 	string addressName;
 };
