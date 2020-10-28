ba229983cfb534b72b9aeea6c6a25293598000e3
Ryan <17rcp@queensu.ca>
2020-10-28 12:10:39 -0400

    Cleaned up inconstistencies across all classes and implemented password validation

---
+++ location.cpp
@@ -0,0 +1,149 @@
+/*
+ * File: Location.h
+ * Date: Oct 24, 2020
+ * Author: Shiyan Boxer, Sean Pollen
+ * Description: Location cpp file
+ * Attributes:
+    * location: Address
+    * maxCapacity: int
+    * currentCapacity: int
+    * indoors: string
+    * internet: bool
+    * volume: int
+    * power: bool
+    * equipment: bool
+* Methods
+    * checkPreferences(User):
+    * getMaxCapacity(): int
+    * getCurrentCapacity(): int
+    * getIndoors(): string
+    * getInternet():bool
+    * getVolume(): int
+    * getPower(): bool
+    * getEquipment(): int
+    * getLocation(): string
+    * seMaxCapacity():
+    * setCurrentCapacity():
+    * setIndoors():
+    * setInternet():
+    * setVolume():
+    * setPower():
+    * setEquipment():
+    * setLocation():
+ */
+/*
+
+toDo
+- change mutators in .h to void
+- write check user preference method
+*/
+
+// Include libraries
+#include <time.h>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Include insult location.h and address.h files
+#include "location.h"
+#include "address.h"
+#include "user.h"
+
+using namespace std;
+
+// Constructors
+Location::Location() : volume(), location(), indoor(), internet(), power(), equipment(), maxCapacity(), currentCapacity() {}
+Location::Location(int volume, Address location, bool indoor, bool internet, bool power, bool equipment, int maxCapacity, int currentCapacity)
+{
+    this->volume = volume;
+    this->location = location;
+    this->indoor = indoor;
+    this->internet = internet;
+    this->power = power;
+    this->equipment = equipment;
+    this->maxCapacity = maxCapacity;
+    this->currentCapacity = currentCapacity;
+}
+// Accessor
+int Location::getVolume() {
+    return volume;
+}
+
+Address Location::getLocation() {
+    return location;
+}
+
+bool Location::getInternet() {
+    return internet;
+}
+
+bool Location::getPower() {
+    return power;
+}
+
+bool Location::getEquipment() {
+    return equipment;
+}
+
+bool Location::getIndoor() {
+    return indoor;
+}
+
+int Location::getMaxCapacity() {
+    return maxCapacity;
+}
+
+int Location::getCurrentCapacity()
+{
+    return currentCapacity;
+}
+
+// Mutators
+void Location::setInternet(bool internet) {
+    this->internet = internet;
+}
+
+void Location::setPower(bool power) {
+    this->power = power;
+}
+
+void Location::setEquipment(bool equipment) {
+    this->equipment = equipment;
+}
+
+void Location::setIndoor(bool indoor)
+{
+    this->indoor = indoor;
+}
+
+void Location::setMaxCapacity(int maxCapacity)
+{
+    this->maxCapacity = maxCapacity;
+}
+
+void Location::setCurrentCapacity(int currentCapacity)
+{
+    this->currentCapacity = currentCapacity;
+}
+
+// Check user preferences method, returns true if the study spot is valid to add to the list
+bool Location::checkPreferences(User user) {
+    //check all the boolean variables, statement is true if every variable matches
+    if (user.getEquipment() == this->equipment
+        && user.getIndoor() == this->indoor
+        && user.getInternet() == this->internet
+        && user.getPower() == this->power
+        && user.getVolume() >= this->volume
+        && this->currentCapacity < this->maxCapacity) {
+        float temp = user.getCurrentLocation().calculateDistance(this->getLocation().getLongitude(), this->getLocation().getLatitude(),
+        		user.getCurrentLocation().getLongitude(), user.getCurrentLocation().getLatitude()); //this function must be overloaded to the address class
+        if (temp <= user.getMaxDistance())
+            return true; // a location exists that meets requirements and is within a valid distance
+        return false; //the location is too far
+    }
+    return false; //nothing meets the requirements
+}
---
+++ user.h
@@ -0,0 +1,52 @@
+/*
+ * Header file for user
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "login.h"
+
+using namespace std;
+
+class User {
+
+public:
+
+	// constructors
+	User(Login loginCreds);
+	User(int volume, Address currentLocation, bool indoor, bool internet, bool power, bool equipment, double maxTravelDistance, Login loginCreds);
+
+	// accessors
+	int getVolume();
+	Address getCurrentLocation();
+	bool getInternet();
+	bool getPower();
+	bool getEquipment();
+	bool getIndoor();
+	double getMaxDistance();
+	Login getLogin();
+
+	// mutators
+	void setVolume(int volume);
+	void setCurrentLocation(Address currentLocation);
+	void setLogin(Login LoginCreds);
+	void setMaxDistance(double maxTravelDistance);
+	void setIndoor(bool indoor);
+	void setEquipment(bool equipment);
+	void setPower(bool power);
+	void setInternet(bool internet);
+
+private:
+
+	// attributes
+	int volume;
+	Address currentLocation;
+	bool indoor;
+	bool internet;
+	bool power;
+	bool equipment;
+	double maxTravelDistance;
+	Login loginCreds;
+};
