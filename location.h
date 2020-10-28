8f328ad51eeae8b6cb2364224925b07e918cb027
Sydney Caulfeild <17sc94@caslab.queensu.ca>
2020-10-28 14:16:48 -0400

    Override the less than operator to provide a custom comparator for sorting.

---
+++ location.h
@@ -0,0 +1,55 @@
+/*
+ * Header file for location
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "user.h"
+
+using namespace std;
+
+class Location {
+
+public:
+
+	// constructors
+	Location();
+	Location(int volume, Address location, string inOrOut, bool internet, bool power, bool equipment, int maxCapacity, int currentCapacity);
+
+	// accessors
+	int getVolume();
+	Address getLocation();
+	bool getInternet();
+	bool getPower();
+	bool getEquipment();
+	bool getIndoor();
+	int getMaxCapacity();
+	int getCurrentCapacity();
+
+	// mutators
+	void setInternet(bool internet);
+	void setPower(bool power);
+	void setEquipment(bool equipment);
+	void setIndoor(bool indoor);
+	void setMaxCapacity(int maxCapacity);
+	void setCurrentCapacity(int currentCapacity);
+
+	// overridden less than operator for sorting
+	bool operator<(const Location& right);
+
+	void checkPreferences(User user);
+
+private:
+
+	// attributes
+	int volume;
+	Address location;
+	bool indoor;
+	bool internet;
+	bool power;
+	bool equipment;
+	int maxCapacity;
+	int currentCapacity;
+};
