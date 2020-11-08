f7665827267c66e000ad3f71a2998b3d37b8eb5a
Sean Pollen <17smp3@caslab.queensu.ca>
2020-11-08 14:43:21 -0500

    Added function to compare list of locations to user preferences

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
 	string enteredUserName;
 	Login newUser = Login();
 	bool validity;
-	
+
 	cout << "Enter userName" << endl;
 	cin >> enteredUserName;
 	newUser.setUserName(enteredUserName);
@@ -27,12 +27,9 @@ int main() {
 	} while (!validity);
 
 	newUser.setPassword(enteredPassword);
-	
-	
-
 
 	list <User> users;	//not going to worry about user lookup efficiency since only need sample of users for proof of concept.
-	User u1(Login("admin","Admin100")); User u2(Login("cisc320","McLeod123")); User u3(Login("anything","Anything1")); //Creating Users
+	User u1(Login("admin", "Admin100")); User u2(Login("cisc320", "McLeod123")); User u3(Login("anything", "Anything1")); //Creating Users
 	users.push_back(u1); users.push_back(u2); users.push_back(u3);	//Adding users to database
 	User currentUser;
 	User nullUser;	//null user will have no attributes set, a non-null user will have at least one attribute set => not equal to make sure logged in
@@ -47,30 +44,43 @@ int main() {
 
 	//check if username is registered
 	for (User const& i : users) {
-		    if(((Login) (i.getLogin)).getUserName() == userName) {
-		    	userExists = true;	//Validates credentials and assigns instance to correct user
-		    }
+		if (((Login)(i.getLogin)).getUserName() == userName) {
+			userExists = true;	//Validates credentials and assigns instance to correct user
 		}
-
-	//username exists
-	if(userExists == true){
-		Login enteredCreds = Login(userName,password);
-			for (User const& i : users) {
-			    if(i.getLogin == enteredCreds) {
-			    	currentUser = i;	//Validates credentials and assigns instance to correct user
-			    }
-			}
-			if(currentUser == nullUser) {	//incorrect password
-				cout << "\nIncorrect Password!" << endl;
-			}
 	}
 
-	while(currentUser != nullUser){
+	//username exists
+	if (userExists == true) {
+		Login enteredCreds = Login(userName, password);
+		for (User const& i : users) {
+			if (i.getLogin == enteredCreds) {
+				currentUser = i;	//Validates credentials and assigns instance to correct user
+			}
+		}
+		if (currentUser == nullUser) {	//incorrect password
+			cout << "\nIncorrect Password!" << endl;
+		}
+	}
+
+	while (currentUser != nullUser) {
 		//functionality available to user once logged in
 	}
 
+	//initialize the list
 
+	list <Location> allLocations; //list of all locations
+	list <Location> validLocations; //list of locations that meet the user specified criteria
+	allLocations = initialize.createList(char filePath[]); //return a list of all locations (reading data from input file)
+	validLocations = createValidList(allLocations, user); //return a list of all locations that meet the preference criteria
+	
 }
 
-
+list<Location> createValidList(list<Location> allLocations, User u) { //function to return a list of valid locations for a given user u
+	list<Location> validLocations;
+	for (std::list<Location>::const_iterator it = allLocations.begin(); it != allLocations.end(); ++it) { //iterate through all locations
+		if (it.checkPreferences(u) == true)
+			validLocations.push_back(it);
+	}
+	return validLocations;
+}
 
