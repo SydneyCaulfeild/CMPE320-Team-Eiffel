d8ec8ea5fba6371229c566009fd1aa56711c2b48
Shiyan Boxer <17sb79@queensu.ca>
2020-11-21 17:50:01 -0500

    update prefences UI

--- preferences.cpp
+++ preferences.cpp
@@ -1,15 +1,35 @@
 #include "preferences.h"
 #include "ui_preferences.h"
+#include "address.h"
+#include "location.h"
 
-//Preferences::Preferences(QWidget *parent):
-//    QMainWindow(parent),
-//    ui(new Ui::Preferences)
-//{
-//    ui->setupUi(this);
-//}
+#include <QCheckBox>
+#include <QMessageBox>
 
-//Preferences::~Preferences()
-//{
-//    delete ui;
-//}
+Preferences::Preferences(QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::Preferences)
+{
+    ui->setupUi(this);
+}
 
+Preferences::~Preferences()
+{
+    delete ui;
+}
+
+Preferences::savePreference()
+{
+    // get all prefences
+    bool power = ui->powerCheck->isChecked();
+    bool outside = ui->outsideCheck->isChecked();
+    bool internet = ui->internetCheck->isChecked();
+    bool equipment = ui->equipmentCheck->isChecked();
+    int volume = ui->volumeCheck->value();
+    int distance = ui->maxDistance->value();
+}
+
+Preferences::displayLocations()
+{
+
+}
